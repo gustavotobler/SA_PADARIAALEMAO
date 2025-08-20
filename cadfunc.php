7143a473caf248f661ad8a4c186dc905179fd6f5
Júlia Pavlick <juliapavlickk@gmail.com>
2025-08-20 14:06:27 -0300

    .

--- cadfunc.php
+++ cadfunc.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Cadastro de Funcionário</title>
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="css/cadfunc.php">
   <link href="css/cadfunc.css" rel="stylesheet"/>
   
 </head>
