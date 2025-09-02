f8c6757d13338113adecd9ac53678633137518f7
gustavotobler <gustavotobler70@gmail.com>
2025-09-02 14:45:08 -0300

    update
    update

--- bancodedados_phpmyadmin/padariadoalemao.sql
+++ bancodedados_phpmyadmin/padariadoalemao.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 02/09/2025 às 19:28
+-- Tempo de geração: 02/09/2025 às 19:44
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -95,6 +95,7 @@ CREATE TABLE `funcionario` (
   `Num_casa` int(11) DEFAULT NULL,
   `Logradouro` varchar(60) DEFAULT NULL,
   `Senha` varchar(255) NOT NULL,
+  `senha_temporaria` tinyint(1) NOT NULL,
   `Email` varchar(60) NOT NULL,
   `nivel_de_acesso` int(1) DEFAULT NULL,
   `Data_nascimento` date DEFAULT NULL,
@@ -106,10 +107,10 @@ CREATE TABLE `funcionario` (
 -- Despejando dados para a tabela `funcionario`
 --
 
-INSERT INTO `funcionario` (`ID_func`, `Nome_func`, `Telefone`, `Sexo`, `RG`, `CPF`, `Esta_civil`, `UF`, `Cidade`, `Bairro`, `CEP`, `Num_casa`, `Logradouro`, `Senha`, `Email`, `nivel_de_acesso`, `Data_nascimento`, `Data_admissao`, `Cargo`) VALUES
-(1, 'Kerry King', '(47) 99685-5520', 'Masculino', '01.203.4013', '141.554.709-26', 'Casado', 'SP', 'São Paulo', 'Centro', '89230-45', 190, 'Rua 25 de março', '$2y$10$zrHZZaNu.qCLV1noijw2teTQVtNHG84cc0InEEL1fOjUIz5iEicMK', 'kerryking@padaria.com', 1, '0000-00-00', '0000-00-00', 'Gerente'),
-(2, 'Ian Lucas Borba', '(92) 03123-1321', 'Masculino', '01.203.4013', '193.239.402-32', 'Viúvo', 'Sa', 'Joinville', 'Espinheiros', '8922687', 189, 'rua', '$2y$10$i.FGwrLPX5JIt9jk98EXoOWOdhj2Yc82rVbFfvh7P33.pYz6uDdp2', 'ian@gmail.com', 0, '0000-00-00', '0000-00-00', 'Padeiro'),
-(3, 'Lucas Borba', '(51) 98765-4321', '', '123456789', '123.456.789-00', 'Solteiro', 'RS', 'Porto Alegre', 'Centro', '90000-00', 101, 'Rua das Flores', 'SenhaForte@123', 'lucas.borba@email.com', 2, '1990-05-15', '2025-08-01', 'Analista de Sistemas');
+INSERT INTO `funcionario` (`ID_func`, `Nome_func`, `Telefone`, `Sexo`, `RG`, `CPF`, `Esta_civil`, `UF`, `Cidade`, `Bairro`, `CEP`, `Num_casa`, `Logradouro`, `Senha`, `senha_temporaria`, `Email`, `nivel_de_acesso`, `Data_nascimento`, `Data_admissao`, `Cargo`) VALUES
+(1, 'Kerry King', '(47) 99685-5520', 'Masculino', '01.203.4013', '141.554.709-26', 'Casado', 'SP', 'São Paulo', 'Centro', '89230-45', 190, 'Rua 25 de março', '$2y$10$fuA8rkHPyIaz0OzwDbL0/.2pyghS0c0c5Eow6t0ok3BWgfaH3azZa', 1, 'kerryking@padaria.com', 1, '0000-00-00', '0000-00-00', 'Gerente'),
+(2, 'Ian Lucas Borba', '(92) 03123-1321', 'Masculino', '01.203.4013', '193.239.402-32', 'Viúvo', 'Sa', 'Joinville', 'Espinheiros', '8922687', 189, 'rua', '$2y$10$i.FGwrLPX5JIt9jk98EXoOWOdhj2Yc82rVbFfvh7P33.pYz6uDdp2', 0, 'ian@gmail.com', 0, '0000-00-00', '0000-00-00', 'Padeiro'),
+(3, 'Lucas Borba', '(51) 98765-4321', '', '123456789', '123.456.789-00', 'Solteiro', 'RS', 'Porto Alegre', 'Centro', '90000-00', 101, 'Rua das Flores', 'SenhaForte@123', 0, 'lucas.borba@email.com', 2, '1990-05-15', '2025-08-01', 'Analista de Sistemas');
 
 -- --------------------------------------------------------
 
