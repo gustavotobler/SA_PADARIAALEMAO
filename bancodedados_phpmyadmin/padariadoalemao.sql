25d588a3ffe046d9408da259473f64c627f626c6
gustavotobler <gustavotobler70@gmail.com>
2025-09-05 17:02:28 -0300

    UPD
    ;;;

--- bancodedados_phpmyadmin/padariadoalemao.sql
+++ bancodedados_phpmyadmin/padariadoalemao.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 05/09/2025 às 21:55
+-- Tempo de geração: 05/09/2025 às 22:01
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
